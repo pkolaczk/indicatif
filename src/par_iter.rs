c716edb7cc48ed91a2ea1530a2dc47c8e3b41fe8
mibac138 <5672750+mibac138@users.noreply.github.com>
2020-04-21 13:38:27 +0200

    Allow limiting updates to ProgressBar when using ParallelProgressIter

--- src/par_iter.rs
+++ src/par_iter.rs
@@ -2,6 +2,8 @@ use crate::ProgressBar;
 use rayon::iter::{
     plumbing::Consumer, plumbing::Folder, plumbing::UnindexedConsumer, ParallelIterator,
 };
+use std::sync::Arc;
+use std::sync::atomic::{AtomicU64, Ordering};
 
 /// Wraps a Rayon parallel iterator.
 ///
@@ -22,31 +24,68 @@ where
     }
 }
 
+#[derive(Clone, Debug)]
+struct IterData(ProgressBar, Arc<(/*update_delta*/ AtomicU64, /*update_counter*/ AtomicU64)>);
+impl IterData {
+    fn new(pb: ProgressBar) -> Self {
+        Self(pb, Arc::new((AtomicU64::new(0), AtomicU64::new(0))))
+    }
+
+    fn set_update_delta(&mut self, delta: u64) {
+        (self.1).0.store(delta, Ordering::SeqCst);
+    }
+
+    fn update(&self, inc: u64) {
+        let (delta, counter) = &*self.1;
+        let delta = delta.load(Ordering::Acquire);
+        let update = loop {
+            let mut update = 0;
+            let old_val = counter.load(Ordering::Acquire);
+            let mut val = old_val;
+            val += inc;
+            if val > delta {
+                update = val / delta;
+                val %= delta;
+            }
+            if counter.compare_and_swap(old_val, val, Ordering::AcqRel) == old_val {
+                break update;
+            }
+        };
+        if update > 0 {
+            self.0.inc(delta * update);
+        }
+    }
+}
+
 pub struct ParProgressBarIter<T> {
     it: T,
-    progress: ProgressBar,
+    data: IterData,
 }
 
 impl<T> ParProgressBarIter<T> {
     pub(crate) fn new(it: T, progress: ProgressBar) -> Self {
-        Self { it, progress }
+        Self { it, data: IterData::new(progress) }
+    }
+
+    pub fn set_update_delta(&mut self, delta: u64) {
+        self.data.set_update_delta(delta);
     }
 }
 
 impl<S: Send, T: ParallelIterator<Item = S>> ParallelProgressIterator for T {
     fn progress_with(self, progress: ProgressBar) -> ParProgressBarIter<Self> {
-        ParProgressBarIter { it: self, progress }
+        ParProgressBarIter::new(self, progress)
     }
 }
 
 struct ProgressConsumer<C> {
     base: C,
-    progress: ProgressBar,
+    data: IterData,
 }
 
 impl<C> ProgressConsumer<C> {
-    fn new(base: C, progress: ProgressBar) -> Self {
-        ProgressConsumer { base, progress }
+    fn new(base: C, data: IterData) -> Self {
+        ProgressConsumer { base, data }
     }
 }
 
@@ -58,8 +97,8 @@ impl<T, C: Consumer<T>> Consumer<T> for ProgressConsumer<C> {
     fn split_at(self, index: usize) -> (Self, Self, Self::Reducer) {
         let (left, right, reducer) = self.base.split_at(index);
         (
-            ProgressConsumer::new(left, self.progress.clone()),
-            ProgressConsumer::new(right, self.progress),
+            ProgressConsumer::new(left, self.data.clone()),
+            ProgressConsumer::new(right, self.data),
             reducer,
         )
     }
@@ -67,7 +106,7 @@ impl<T, C: Consumer<T>> Consumer<T> for ProgressConsumer<C> {
     fn into_folder(self) -> Self::Folder {
         ProgressFolder {
             base: self.base.into_folder(),
-            progress: self.progress,
+            data: self.data,
         }
     }
 
@@ -78,7 +117,7 @@ impl<T, C: Consumer<T>> Consumer<T> for ProgressConsumer<C> {
 
 impl<T, C: UnindexedConsumer<T>> UnindexedConsumer<T> for ProgressConsumer<C> {
     fn split_off_left(&self) -> Self {
-        ProgressConsumer::new(self.base.split_off_left(), self.progress.clone())
+        ProgressConsumer::new(self.base.split_off_left(), self.data.clone())
     }
 
     fn to_reducer(&self) -> Self::Reducer {
@@ -88,17 +127,17 @@ impl<T, C: UnindexedConsumer<T>> UnindexedConsumer<T> for ProgressConsumer<C> {
 
 struct ProgressFolder<C> {
     base: C,
-    progress: ProgressBar,
+    data: IterData,
 }
 
 impl<T, C: Folder<T>> Folder<T> for ProgressFolder<C> {
     type Result = C::Result;
 
     fn consume(self, item: T) -> Self {
-        self.progress.inc(1);
+        self.data.update(1);
         ProgressFolder {
             base: self.base.consume(item),
-            progress: self.progress,
+            data: self.data,
         }
     }
 
@@ -115,7 +154,7 @@ impl<S: Send, T: ParallelIterator<Item = S>> ParallelIterator for ParProgressBar
     type Item = S;
 
     fn drive_unindexed<C: UnindexedConsumer<Self::Item>>(self, consumer: C) -> C::Result {
-        let consumer1 = ProgressConsumer::new(consumer, self.progress);
+        let consumer1 = ProgressConsumer::new(consumer, self.data);
         self.it.drive_unindexed(consumer1)
     }
 }
